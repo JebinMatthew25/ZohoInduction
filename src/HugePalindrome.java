721dcb009560a613f3e8c915a42567994b9f2ebc
jebin-pt2523 <Jeswin2008>
2018-12-13 10:45:17 +0530

    Huge Update

--- src/HugePalindrome.java
+++ src/HugePalindrome.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HugePalindrome {
+    String seq;
+    Map<Character, Integer> count = new HashMap<Character, Integer>();
+    Character middle;
+    String palindrome;
+
+    HugePalindrome(String seq)
+    {
+        this.seq = seq;
+        for (int i=0; i<seq.length(); i++)
+        {
+            char k = seq.charAt(i);
+            if(count.containsKey(k))
+                count.put(k, count.get(k)+1);
+            else
+                count.put(k, 1);
+        }
+    }
+
+    void show_count()
+    {
+        for (Character ch: count.keySet())
+            System.out.println(ch.toString() + ' ' + count.get(ch));
+        System.out.println();
+    }
+
+    void pre_process()
+    {
+        Boolean got = false;
+        for (Character ch: count.keySet())
+        {
+            if(got)
+                count.put(ch, count.get(ch)&0xFE);
+            else if((count.get(ch) & 1) == 1)
+            {
+                got = true;
+                middle = ch;
+                count.put(ch, count.get(ch)&0xFE);
+            }
+        }
+    }
+
+    void show_palindrome()
+    {
+        if(palindrome.equals(""))
+            System.out.println("Make the palindrome by calling make_palindrome!");
+        else
+            System.out.println(palindrome);
+    }
+
+    void make_palindrome()
+    {
+        StringBuilder left = new StringBuilder();
+
+        for (Character ch: count.keySet())
+            for (int i =0; i< count.get(ch)/2; i++)
+                if(ch != ' ')
+                    left.append(ch);
+                else break;
+
+        palindrome = left.toString() + middle + left.reverse();
+    }
 }
--- src/HugePalindromeDriver.java
+++ src/HugePalindromeDriver.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class HugePalindromeDriver {
+    public static void main(String[] args)
+    {
+        HugePalindrome obj = new HugePalindrome("Find the largest palindrome");
+        obj.show_count();
+        obj.pre_process();
+        obj.show_count();
+        obj.make_palindrome();
+        obj.show_palindrome();
+    }
 }
